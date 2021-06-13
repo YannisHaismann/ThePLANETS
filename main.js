1eb1f23b4b93bef18991e613855ff857f09f7dc8
Yannis Haismann <yannishaismann@outlook.fr>
2021-06-13 18:30:29 +0200

    Size imgs js

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ function readJsonFile(file){
         rawFile.send();
     })
 }
+var tablet = 1025;
+var phone = 376;
 var file    = 'data.json';
 let nav     = document.querySelector("#nav");
 
@@ -21,10 +23,13 @@ nav.addEventListener("click", (e) =>{
 
     readJsonFile(file).then(function (response){
 
+        console.log(window.innerWidth);
+
         let planet = JSON.parse(response);
         let planetImg, planetName, planetGeologyImg, planetDescription, planetWikipedia, planetRotationTime, planetRevolutionTime,
-        planetRadius, planetAverageTemp;
+        planetRadius, planetAverageTemp, planetWidthImg;
         let cursor = 0;
+        let windowWidth = window.innerWidth;
 
         let zoomImg = document.querySelector("#img-geology");
         if(zoomImg.style.display != "none"){
@@ -34,30 +39,43 @@ nav.addEventListener("click", (e) =>{
         switch(e.target.textContent){
             case "MERCURY":
                 cursor = 0;
+                width = 300;
                 break;
             case "VENUS":
                 cursor = 1;
+                width = 450;
                 break;
             case "EARTH":
                 cursor = 2;
+                width = 500;
                 break;
             case "MARS":
                 cursor = 3;
+                width = 400;
                 break;
             case "JUPITER":
                 cursor = 4;
+                width = 700;
                 break;
             case "SATURN":
                 cursor = 5;
+                width = 700;
                 break;
             case "URANUS":
                 cursor = 6;
+                width = 550;
                 break;
             case "NEPTUNE":
                 cursor = 7;
+                width = 600;
                 break;
             default:
                 cursor = -1;
+        }
+        if(windowWidth > phone && windowWidth < tablet){
+            width = (width / 3) * 2;
+        }else if(windowWidth < phone){
+            width = width / 3
         }
     if(cursor >= 0) {
         planetImg = planet[cursor].images.planet;
@@ -72,6 +90,7 @@ nav.addEventListener("click", (e) =>{
 
         let mainImg = document.querySelector("#main-img");
         mainImg.style.backgroundImage = "url(" + planetImg + ")";
+        mainImg.style.backgroundSize = width + "px";
 
         let geologyImg = document.querySelector("#img-geology");
         geologyImg.setAttribute("src", planetGeologyImg);
@@ -110,7 +129,11 @@ choiceView.addEventListener("click", (e) =>{
     let overviewElt = document.getElementById("overview");
     let internalStructureElt = document.getElementById("internal-structure");
     let surfaceGeologyElt = document.getElementById("surface-geology");
-    if(e.target.textContent == "OVERVIEW" || e.target.textContent == "INTERNAL STRUCTURE" || e.target.textContent == "SURFACE GEOLOGY" || e.target.textContent == "01"  || e.target.textContent == "02" || e.target.textContent == "03"
+    let structure = document.getElementById("structure");
+    let surface = document.getElementById("surface");
+    let internal = document.getElementById("internal");
+    let geology = document.getElementById("geology");
+    if(e.target.textContent == "OVERVIEW" || e.target == structure || e.target == surface || e.target == internal || e.target == geology || e.target.textContent == "01"  || e.target.textContent == "02" || e.target.textContent == "03"
     || e.target == overviewElt || e.target == internalStructureElt || e.target == surfaceGeologyElt){
         readJsonFile(file).then(function (response) {
             let planet = JSON.parse(response);
@@ -154,7 +177,7 @@ choiceView.addEventListener("click", (e) =>{
                 planetText = planet[cursor].overview.content;
                 planetWikipedia = planet[cursor].overview.source;
 
-            } else if (e.target.textContent == "INTERNAL STRUCTURE" || e.target.textContent == "02" || e.target == internalStructureElt) {
+            } else if (e.target.textContent == "INTERNAL STRUCTURE" || e.target.textContent == "02" || e.target == internalStructureElt || e.target == internal || e.target == structure) {
 
                 if (zoomImg.style.display != "none") {
                     zoomImg.style.display = "none";
@@ -164,7 +187,7 @@ choiceView.addEventListener("click", (e) =>{
                 planetText = planet[cursor].structure.content;
                 planetWikipedia = planet[cursor].structure.source;
 
-            } else if (e.target.textContent == "SURFACE GEOLOGY" || e.target.textContent == "03" || e.target == surfaceGeologyElt) {
+            } else if (e.target.textContent == "SURFACE GEOLOGY" || e.target.textContent == "03" || e.target == surfaceGeologyElt || e.target == surface || e.target == geology) {
                 if (zoomImg.style.display == "none") {
                     zoomImg.style.display = "block";
                 }
