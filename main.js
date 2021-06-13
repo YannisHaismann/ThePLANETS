aac2ffa8b1fa7a4829a645c279304cdcba4ab74c
Yannis Haismann <yannishaismann@outlook.fr>
2021-06-13 22:27:59 +0200

    Buttons hoover + refresh phone

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+var resizeTimeout;
+window.addEventListener('resize', function(event) {
+    if(window.innerWidth < 420 && window.innerWidth > 370){
+        clearTimeout(resizeTimeout);
+        resizeTimeout = setTimeout(function(){
+            window.location.reload();
+        }, 1000);
+    }
+});
+
 function readJsonFile(file){
     return new Promise(function(resolve, reject){
         var rawFile = new XMLHttpRequest();
@@ -14,10 +24,11 @@ function readJsonFile(file){
         rawFile.send();
     })
 }
-var tablet  = 1025;
-var phone   = 376;
-var file    = 'data.json';
-let nav     = document.querySelector("#nav");
+var tablet      = 1025;
+var phone       = 376;
+var file        = 'data.json';
+var windowWidth = window.innerWidth;
+let nav         = document.querySelector("#nav");
 
 nav.addEventListener("click", (e) =>{
 
@@ -29,8 +40,11 @@ nav.addEventListener("click", (e) =>{
         let planetImg, planetName, planetGeologyImg, planetDescription, planetWikipedia, planetRotationTime, planetRevolutionTime,
         planetRadius, planetAverageTemp, width, geologyWidth;
         let cursor = 0;
-        let windowWidth = window.innerWidth;
-
+        let overview            = document.getElementById("overview");
+        let internalStructure   = document.getElementById("internal-structure");
+        let surfaceGeology      = document.getElementById("surface-geology");
+        let transparent         = "rgba(0, 0, 0, 0)"
+        let basicBorderBottom   = "1px solid rgb(56, 56, 79)";
         let zoomImg = document.querySelector("#img-geology");
         if(zoomImg.style.display != "none"){
             zoomImg.style.display = "none";
@@ -40,34 +54,42 @@ nav.addEventListener("click", (e) =>{
             case "MERCURY":
                 cursor = 0;
                 width = 300;
+                color = "rgba(65, 158, 187, 1)";
                 break;
             case "VENUS":
                 cursor = 1;
                 width = 450;
+                color = "rgba(237, 162, 73, 1)";
                 break;
             case "EARTH":
                 cursor = 2;
                 width = 500;
+                color = "rgba(109, 46, 213, 1)";
                 break;
             case "MARS":
                 cursor = 3;
                 width = 400;
+                color = "rgba(209, 76, 50, 1)";
                 break;
             case "JUPITER":
                 cursor = 4;
                 width = 700;
+                color = "rgba(216, 58, 52, 1)";
                 break;
             case "SATURN":
                 cursor = 5;
                 width = 700;
+                color = "rgba(205, 81, 32, 1)";
                 break;
             case "URANUS":
                 cursor = 6;
                 width = 550;
+                color = "rgba(30, 193, 162, 1)";
                 break;
             case "NEPTUNE":
                 cursor = 7;
                 width = 600;
+                color = "rgba(45, 104, 240, 1)";
                 break;
             default:
                 cursor = -1;
@@ -78,16 +100,33 @@ nav.addEventListener("click", (e) =>{
             width = width / 3;
         }
         geologyWidth = width / 2;
+
+        if(windowWidth > phone){
+            internalStructure.style.backgroundColor = transparent;
+            surfaceGeology.style.backgroundColor    = transparent;
+            overview.style.backgroundColor          = color;
+            surfaceGeology.style.borderBottom       = basicBorderBottom;
+            internalStructure.style.borderBottom    = basicBorderBottom;
+            overview.style.borderBottom             = basicBorderBottom;
+        }else{
+            internalStructure.style.backgroundColor = transparent;
+            surfaceGeology.style.backgroundColor    = transparent;
+            overview.style.backgroundColor          = transparent;
+            surfaceGeology.style.borderBottom       = "none";
+            internalStructure.style.borderBottom    = "none";
+            overview.style.borderBottom             = "4px solid " + color;
+        }
+
     if(cursor >= 0) {
-        planetImg = planet[cursor].images.planet;
-        planetGeologyImg = planet[cursor].images.geology;
-        planetName = planet[cursor].name;
-        planetDescription = planet[cursor].overview.content;
-        planetWikipedia = planet[cursor].overview.source;
-        planetRotationTime = planet[cursor].rotation;
-        planetRevolutionTime = planet[cursor].revolution;
-        planetRadius = planet[cursor].radius;
-        planetAverageTemp = planet[cursor].temperature;
+        planetImg               = planet[cursor].images.planet;
+        planetGeologyImg        = planet[cursor].images.geology;
+        planetName              = planet[cursor].name;
+        planetDescription       = planet[cursor].overview.content;
+        planetWikipedia         = planet[cursor].overview.source;
+        planetRotationTime      = planet[cursor].rotation;
+        planetRevolutionTime    = planet[cursor].revolution;
+        planetRadius            = planet[cursor].radius;
+        planetAverageTemp       = planet[cursor].temperature;
 
         let mainImg = document.querySelector("#main-img");
         mainImg.style.backgroundImage = "url(" + planetImg + ")";
@@ -128,55 +167,94 @@ nav.addEventListener("click", (e) =>{
 let choiceView = document.querySelector("#choice-view");
 choiceView.addEventListener("click", (e) =>{
     console.log(e.target);
-    let overviewElt = document.getElementById("overview");
-    let internalStructureElt = document.getElementById("internal-structure");
-    let surfaceGeologyElt = document.getElementById("surface-geology");
-    let structure = document.getElementById("structure");
-    let surface = document.getElementById("surface");
-    let internal = document.getElementById("internal");
-    let geology = document.getElementById("geology");
+    let overviewElt             = document.getElementById("overview");
+    let internalStructureElt    = document.getElementById("internal-structure");
+    let surfaceGeologyElt       = document.getElementById("surface-geology");
+    let structure               = document.getElementById("structure");
+    let surface                 = document.getElementById("surface");
+    let internal                = document.getElementById("internal");
+    let geology                 = document.getElementById("geology");
+    let overview                = document.getElementById("overview");
+    let internalStructure       = document.getElementById("internal-structure");
+    let surfaceGeology          = document.getElementById("surface-geology");
     if(e.target.textContent == "OVERVIEW" || e.target == structure || e.target == surface || e.target == internal || e.target == geology || e.target.textContent == "01"  || e.target.textContent == "02" || e.target.textContent == "03"
     || e.target == overviewElt || e.target == internalStructureElt || e.target == surfaceGeologyElt){
         readJsonFile(file).then(function (response) {
-            let planet = JSON.parse(response);
-            let mainTitle = document.querySelector("#main-title");
-            let planetImg, planetText, planetWikipedia;
+            let planet      = JSON.parse(response);
+            let mainTitle   = document.querySelector("#main-title");
+            let planetImg, planetText, planetWikipedia, color;
+            let transparent = "rgba(0, 0, 0, 0)";
+            let basicBorderBottom   = "1px solid rgb(56, 56, 79)";
+            let lowWhite            = "rgba(255, 255, 255, 0.6)";
+            let white               = "rgba(255, 255, 255, 1)";
 
             switch (mainTitle.textContent) {
                 case "MERCURY":
                     cursor = 0;
+                    color = "rgba(65, 158, 187, 1)";
                     break;
                 case "VENUS":
                     cursor = 1;
+                    color = "rgba(237, 162, 73, 1)";
                     break;
                 case "EARTH":
                     cursor = 2;
+                    color = "rgba(109, 46, 213, 1)";
                     break;
                 case "MARS":
                     cursor = 3;
+                    color = "rgba(209, 76, 50, 1)";
                     break;
                 case "JUPITER":
                     cursor = 4;
+                    color = "rgba(216, 58, 52, 1)";
                     break;
                 case "SATURN":
                     cursor = 5;
+                    color = "rgba(205, 81, 32, 1)";
                     break;
                 case "URANUS":
                     cursor = 6;
+                    color = "rgba(30, 193, 162, 1)";
                     break;
                 case "NEPTUNE":
                     cursor = 7;
+                    color = "rgba(45, 104, 240, 1)";
                     break;
             }
+            internalStructure.style.backgroundColor = transparent;
+            surfaceGeology.style.backgroundColor    = transparent;
+            overview.style.backgroundColor          = transparent;
+
+            if(windowWidth > phone){
+                surfaceGeology.style.borderBottom       = basicBorderBottom;
+                internalStructure.style.borderBottom    = basicBorderBottom;
+                overview.style.borderBottom             = basicBorderBottom;
+            }else{
+                overview.style.color                    = lowWhite;
+                surfaceGeology.style.color              = lowWhite;
+                internalStructure.style.color           = lowWhite;
+                surfaceGeology.style.borderBottom       = "none";
+                internalStructure.style.borderBottom    = "none";
+                overview.style.borderBottom             = "none";
+            }
+
             let zoomImg = document.querySelector("#img-geology");
             if (e.target.textContent == "OVERVIEW" || e.target.textContent == "01" || e.target == overviewElt) {
 
                 if (zoomImg.style.display != "none") {
                     zoomImg.style.display = "none";
                 }
+                if(windowWidth > phone){
+                    overview.style.backgroundColor          = color;
+                }else{
+                    overview.style.color                    = white;
+                    overview.style.borderBottom             = "4px solid " + color;
+                }
 
-                planetImg = planet[cursor].images.planet;
-                planetText = planet[cursor].overview.content;
+
+                planetImg       = planet[cursor].images.planet;
+                planetText      = planet[cursor].overview.content;
                 planetWikipedia = planet[cursor].overview.source;
 
             } else if (e.target.textContent == "INTERNAL STRUCTURE" || e.target.textContent == "02" || e.target == internalStructureElt || e.target == internal || e.target == structure) {
@@ -184,17 +262,32 @@ choiceView.addEventListener("click", (e) =>{
                 if (zoomImg.style.display != "none") {
                     zoomImg.style.display = "none";
                 }
+                if(windowWidth > phone){
+                    internalStructure.style.backgroundColor = color;
+                }else{
+                    internalStructure.style.color           = white;
+                    internalStructure.style.borderBottom    = "4px solid " + color;
+                }
 
-                planetImg = planet[cursor].images.internal;
-                planetText = planet[cursor].structure.content;
+
+                planetImg       = planet[cursor].images.internal;
+                planetText      = planet[cursor].structure.content;
                 planetWikipedia = planet[cursor].structure.source;
 
             } else if (e.target.textContent == "SURFACE GEOLOGY" || e.target.textContent == "03" || e.target == surfaceGeologyElt || e.target == surface || e.target == geology) {
-                    zoomImg.style.display = "block";
 
-                planetImg = planet[cursor].images.planet;
-                planetText = planet[cursor].geology.content;
-                planetWikipedia = planet[cursor].geology.source;
+                if(windowWidth > phone){
+                    surfaceGeology.style.backgroundColor    = color;
+                }else{
+                    surfaceGeology.style.color              = white;
+                    surfaceGeology.style.borderBottom       = "4px solid " + color;
+                }
+
+
+                zoomImg.style.display    = "block";
+                planetImg                = planet[cursor].images.planet;
+                planetText               = planet[cursor].geology.content;
+                planetWikipedia          = planet[cursor].geology.source;
 
             }
 
